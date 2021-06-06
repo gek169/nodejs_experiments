40f944c010277cee29beaa6e471f87367b1fa9ba
gek <6974902+gek169@users.noreply.github.com>
2021-06-06 15:57:17 -0400

    update

--- helloworld.js
+++ helloworld.js
@@ -5,6 +5,6 @@ var http = require('http');
 http.createServer(function (req,res)
 {
 	res.writeHead(200,{'Content-Type': 'text/plain'});
-	res.end('Heya Gek!');
+	res.end('Heya Gek! <from node!>');
 }
 ).listen(7000);
